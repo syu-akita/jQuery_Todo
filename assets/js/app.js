1c2968db6ffa98a08636f70a80fe26af9afa5e9a
mugimugitkr <mugimugithirty@gmail.com>
2019-05-14 20:52:26 +0900

    no message

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,8 @@
+// ゴミ箱アイコン
+let removeIcon = '<i class="far fa-trash-alt fa-lg"></i>';
+// 完了アイコン
+let doneIcon = '<i class="far fa-check-circle fa-lg"></i>';
+
 // 追加ボタンがクリックされた時
 $('#add').click(function() {
     // 入力欄を取得
@@ -17,6 +22,28 @@ $('#add').click(function() {
 
     // liに入力値を設定
     li.text(task);
+
+    // divの作成
+    let buttons = $('<div>');
+    buttons.addClass('buttons');
+
+    // buttonの作成
+    // 削除ボタン
+    let remove = $('<button>');
+    remove.addClass('remove');
+    remove.html(removeIcon);
+
+    // 完了ボタン
+    let done = $('<button>');
+    done.addClass('done');
+    done.html(doneIcon);
+
+    // divにボタンを追加
+    buttons.append(remove);
+    buttons.append(done);
+
+    // liにdivを追加
+    li.append(buttons);
 
     // NotYetのulにliを追加
     $('#not-yet').prepend(li);
