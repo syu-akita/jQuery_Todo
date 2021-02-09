3f84269c50994306923f805bca3ee803fdfff2f8
syu-akita <syu.akita.0612@gmail.com>
2021-02-09 19:00:43 +0900

    削除ボタン、完了ボタンの作成

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,12 @@
+// ゴミ箱アイコン
+let removeIcon = '<i class="far fa-trash-alt fa-lg"></i>';
+// 完了アイコン
+let doneIcon = '<i class="far fa-check-circle fa-lg"></i>';
+
+// divの作成
+// buttonの作成
+// liにdivを追加
+
 $('#add').click(function(){
   // 入力欄を取得
   let inputTask = $('#task');
@@ -17,6 +26,29 @@ $('#add').click(function(){
   // liに入力値を設定
 
   li.text(task);
+
+  let buttons = $('<div>');
+  buttons.addClass('buttons');
+  
+  //buttonの作成
+
+  let remove = $('<button>');
+  remove.addClass('remove');
+  remove.html(removeIcon);
+
+  let done = $('<button>');
+  done.addClass('done');
+  done.html(doneIcon);
+
+  //buttonの追加
+
+  buttons.append(remove);
+  buttons.append(done);
+
+  //liにdivの追加
+
+  li.append(buttons);
+  
   // NotYetのulにliを追加
   $('#not-yet').prepend(li);
 
